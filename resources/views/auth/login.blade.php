75f772962506b09e7a4699cbcf8be711ac6a62b7
nmaidana <nelson_lmaidana@hotmail.com>
2019-05-31 15:24:16 -0400

    Modificacion de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,41 +1,88 @@
 <!DOCTYPE html>
+@section('adminlte_css')
+<!-- Tell the browser to be responsive to screen width -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+<!-- Theme style -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/css/AdminLTE.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/css/skins/_all-skins.min.css">
+
+<!-- iCheck -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/_all.css">
+@yield('css')
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>InfyOm Laravel Generator</title>
+        <style>
+                .border{
+                    border-color: black;
+                }
+                   .imagencentro{
+                       
+                    margin-left: auto;
+                    margin-right: auto;
+                    display: block;
+                    max-width:100%;
+                    max-height:100%;
+                    margin-top: 50px;
+                    margin-bottom: 23px;
+                   }    
+                   .center{
+                       text-align: center;
+                   }
+                
+                   .total{
+                    font-weight: bold;
+                    
+                }
+                
+                .titulo{
+                    margin-top: 20px;
+                    text-align: center;
+                    margin-bottom: -60px;
+                }
 
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/css/AdminLTE.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/css/skins/_all-skins.min.css">
-
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/_all.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+                @media screen and  (max-width:768px) {
+                .titulo {
+                    position: relative;
+                    top: -17px;
+                    margin-bottom: 5px;
+    }
+    }
+    
+                body {
+                    
+                    background-color: #a1b3d1;
+                }
+        
+                </style>
 </head>
-<body class="hold-transition login-page">
+<body class="body">
+
+        <div class="row">
+                <div class="col-md-4">
+                        <img src="{{asset('img/CASTELLANO-Y-GURANI-min-de-la-vivienda.png')}}" class="imagencentro" width="230" height="70">
+                </div>
+                <div class="col-md-4">
+                        <img src="{{asset('img/gobierno-nacional.png')}}" class="imagencentro"  width="250" height="60">
+                </div>
+                <div class="col-md-4">
+                        <img src="{{asset('img/slogan.png')}}" class="imagencentro" width="220" height="70">
+                </div>
+            </div>
+                <div class="titulo">
+                        <h2><strong> Sistema de Impresión de Subsidios</strong></h2>
+                </div>
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>Registro </b>Formulario</a>
+        <a href="{{ url('/home') }}">
     </div>
 
     <!-- /.login-logo -->
